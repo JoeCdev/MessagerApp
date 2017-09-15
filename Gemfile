78a90e7ce7930f5d9a56b20140ad781503ed9340
Joe Carroll <jcarroll786@gmail.com>
2017-09-15 21:35:49 +0100

    before email notifications

--- Gemfile
+++ Gemfile
@@ -24,6 +24,11 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
 gem 'mailboxer'
+gem 'gravatar_image_tag'
+gem 'chosen-rails'
+
+
+gem 'jquery-turbolinks'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,33 @@
 Rails.application.routes.draw do
+  get 'messages/new'
+
   get 'coversations/index'
 
   devise_for :users
 
-  resources :conversations, only: [:index, :show, :destroy]
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :reply
+    end
+  end
+
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :restore
+    end
+  end
+
+  resources :conversations, only: [:index, :show, :destroy] do
+    collection do
+      delete :empty_trash
+    end
+  end
+
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :mark_as_read
+    end
+  end
 
   root "coversations#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
